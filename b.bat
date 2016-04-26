6d17ecfd541466b20d77f3e912c0e8a09537163a
ChokSheak Lau <choksheak@gmail.com>
2016-04-25 19:00:49 -0700

    Make golint optional for Windows build

--- b.bat
+++ b.bat
@@ -1,11 +1,16 @@
-@echo Running go fmt
-@go fmt github.com\choksheak\findfile\ff
+@echo off
 
-@REM Install golint
-@REM go get -u github.com/golang/lint/golint
+echo Running go fmt
+go fmt github.com\choksheak\findfile\ff
 
-@echo Running golint
-@golint github.com\choksheak\findfile\ff
+where golint >nul 2>&1
+if %ERRORLEVEL% EQU 0 goto do_golint
+    echo Skipping golint (install: go get -u github.com/golang/lint/golint)
+goto after_golint
+:do_golint
+    echo Running golint
+    golint github.com\choksheak\findfile\ff
+:after_golint
 
-@echo Building ff executable
-@go install github.com\choksheak\findfile\ff
+echo Building ff executable
+go install github.com\choksheak\findfile\ff
