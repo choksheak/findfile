5e366296e48783f816944482e53bfcb109e9e808
ChokSheak Lau <choksheak@gmail.com>
2021-12-09 15:03:39 -0800

    Update b.bash for Mac

--- b.bash
+++ b.bash
@@ -1,16 +1,21 @@
 #/bin/bash
 
-# Exit when any command fails.
-set -e
+if [ ! `which go` ]; then
+  echo Please install the go executable.
+  exit 1
+fi
 
 echo Running go fmt
-go fmt github.com/choksheak/findfile/ff
+go fmt ff/...
 
-if [ `which golint1` ]; then
+if [ `which golint` ]; then
   echo Running golint
   golint github.com/choksheak/findfile/ff
+elif [ `which golangci-lint` ]; then
+  echo Running golangci-lint
+  golangci-lint run ff
 else
-  echo "Skipping golint (install: go get -u github.com/golang/lint/golint)"
+  echo "Skipping golint (install: Linux: go install golang.org/x/lint/golint@latest OR Mac: brew install golangci-lint)"
 fi
 
 echo Building ff executable
