f3d833842c726685882f4c8030248b34db477fd6
ChokSheak Lau <choksheak@gmail.com>
2021-12-09 14:44:15 -0800

    Update b.bash

--- b.bash
+++ b.bash
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# Exit when any command fails.
+set -e
+
 echo Running go fmt
 go fmt github.com/choksheak/findfile/ff
 
@@ -11,5 +14,8 @@ else
 fi
 
 echo Building ff executable
-go install github.com/choksheak/findfile/ff
+go install github.com/choksheak/findfile/ff@latest
 
+cd ff; go build
+
+echo "Compiled ff executable in ./ff directory."
